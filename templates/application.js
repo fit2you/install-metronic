1fd7455d5267ac74cc53bfffa0c5b2e96e706e1b
Antonio Ganci <a.ganci@fit2you.it>
2015-12-02 17:02:18 +0100

    removed unused assets

--- templates/application.js
+++ templates/application.js
@@ -31,19 +31,8 @@
 //= require datatables.bootstrap
 
 // Fileupload
-//= require jquery.fancybox.pack
 //= require jquery.ui.widget
-//= require tmpl.min
-//= require load-image.min
-//= require canvas-to-blob.min
-//= require jquery.blueimp-gallery.min
 //= require jquery.iframe-transport
 //= require jquery.fileupload
-//= require jquery.fileupload-process
-//= require jquery.fileupload-image
-//= require jquery.fileupload-audio
-//= require jquery.fileupload-video
-//= require jquery.fileupload-validate
-//= require jquery.fileupload-ui
 
 //= require_tree .
